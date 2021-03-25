15d16f54adf18ea9a0834dabb401a2a874b0649b
Cristobal Torres <smis009220@ugb.edu.sv>
2021-03-25 17:25:05 -0600

    Creacion del HANDLER, construccion en proceso...

--- app/src/main/java/com/example/crud_semana10/DatabaseHandler.java
+++ app/src/main/java/com/example/crud_semana10/DatabaseHandler.java
@@ -1,6 +1,8 @@
 package com.example.crud_semana10;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,10 +10,11 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-
+public DatabaseHandler( @Nullable Context context) {super(context, "sucursal.db", null,1);}
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+    db.execSQL("CREATE TABLE tienda(codTienda TEXT PRIMARY KEY, nombre TEXT, direccion TEXT, ubicacion TEXT)");
 
     }
 
