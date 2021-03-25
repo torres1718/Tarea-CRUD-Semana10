66a93ee152bde29ef8a2cb3e7df8e9620c6d5743
Cristobal Torres <smis009220@ugb.edu.sv>
2021-03-25 17:16:52 -0600

    Initial commit

---
+++ app/src/main/java/com/example/crud_semana10/DatabaseHandler.java
@@ -0,0 +1,22 @@
+package com.example.crud_semana10;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
