e5c8e48c06b22856b355f0310a3762e2c97b08c3
Cristobal Torres <smis009220@ugb.edu.sv>
2021-03-25 18:33:20 -0600

    Activity, mainactivity y handler terminadas

--- app/src/main/java/com/example/crud_semana10/DatabaseHandler.java
+++ app/src/main/java/com/example/crud_semana10/DatabaseHandler.java
@@ -20,6 +20,50 @@ public DatabaseHandler( @Nullable Context context) {super(context, "sucursal.db"
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+db.execSQL("DROP TABLE IF EXISTS tienda");
+    }
+    public Boolean insertData(String codTienda, String nombre, String direccion, String ubicacion){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("codTienda", codTienda);
+        contentValues.put("nombre", nombre);
+        contentValues.put("direccion", direccion);
+        contentValues.put("ubicacion", ubicacion);
+        long result=db.insert("tienda", null, contentValues);
+        if(result==-1){
+            return false;
+        }else{
+            return true;
+        }
 
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db=this.getWritableDatabase();
+        Cursor cursor=db.rawQuery("SELECT*FROM tienda",null);
+        return cursor;
+    }
+    public Boolean updateData(String codTienda, String nombre, String ubicacion, String direccion){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put("codTienda", codTienda);
+        contentValues.put("nombre", nombre);
+        contentValues.put("direccion", direccion);
+        contentValues.put("ubicacion", ubicacion);
+
+        //registro actualizar
+        Cursor cursor=db.rawQuery("SELECT * FROM persona WHERE codTienda=?", new String[]{codTienda});
+
+        if(cursor.getCount()>0) {
+            long result = db.update("persona", contentValues, "codTienda=?", new String[]{codTienda});
+            if (result == -1) {
+                return false;
+
+            } else {
+                return true;
+            }
+        }else{
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/crud_semana10/MainActivity.java
+++ app/src/main/java/com/example/crud_semana10/MainActivity.java
@@ -1,14 +1,90 @@
 package com.example.crud_semana10;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText cod, nombre, direccion, ubicacion;
+    Button ins, up, list;
+    DatabaseHandler DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cod=findViewById(R.id.cod);
+        nombre=findViewById(R.id.nombre);
+        direccion=findViewById(R.id.direccion);
+        ubicacion=findViewById(R.id.ubicacion);
+        ins=findViewById(R.id.btInsert);
+        up=findViewById(R.id.btUpdate);
+        list=findViewById(R.id.btList);
+        DB=new DatabaseHandler(this);
+
+        ins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String codTiendaTXT = cod.getText().toString();
+                String nombreTXT = nombre.getText().toString();
+                String direccionTXT = direccion.getText().toString();
+                String ubicacionTXT = ubicacion.getText().toString();
+                Boolean checkInsert = DB.insertData(codTiendaTXT, nombreTXT, direccionTXT, ubicacionTXT);
+
+                if (checkInsert == true) {
+                    Toast.makeText(MainActivity.this, "Se ha insertado un nuevo dato", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "No se ha insertado el dato", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+
+list.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Cursor result=DB.getData();
+        if(result.getCount()==0){
+            Toast.makeText(MainActivity.this,"Aun no hay registros", Toast.LENGTH_LONG).show();
+            return;
+        }
+        StringBuffer buffer=new StringBuffer();
+        while(result.moveToNext()){
+            buffer.append("Codigo: "+result.getString(0)+"\n");
+            buffer.append("Nombre: "+result.getString(1)+"\n");
+            buffer.append("Direccion: "+result.getString(2)+"\n");
+            buffer.append("Ubicacion: "+result.getString(2)+"\n\n");
+
+        }
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(true);
+        builder.setTitle("Listado de tiendas registradas");
+        builder.setMessage(buffer.toString());
+        builder.show();
+    }
+});
+up.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        String codTiendaTEXT=cod.getText().toString();
+        String nombreTEXT=nombre.getText().toString();
+        String direccionTEXT=direccion.getText().toString();
+        String ubicacionTEXT=ubicacion.getText().toString();
+        Boolean checkInsert=DB.updateData(codTiendaTEXT,nombreTEXT,ubicacionTEXT,direccionTEXT);
+
+        if(checkInsert==true){
+            Toast.makeText(MainActivity.this,"Su dato se actualizo",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(MainActivity.this,"Su dato no se actualizo",Toast.LENGTH_LONG).show();
+
+        }
+    }
+});
     }
 }
